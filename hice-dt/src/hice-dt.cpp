f972dcffa65910d01223870b504488c1e082e028
Tobias Zimmermann <t_zimmerma15@cs.uni-kl.de>
2020-03-13 17:34:31 +0100

    fix: fix a compile error
    A namespace path was incorrect when <filesystem>
    is present

--- hice-dt/src/hice-dt.cpp
+++ hice-dt/src/hice-dt.cpp
@@ -12,7 +12,7 @@
 
 #if __has_include(<filesystem>)
     #include <filesystem>
-    namespace fs = filesystem;
+    namespace fs = std::filesystem;
 #else
     #include <experimental/filesystem>
     namespace fs = std::experimental::filesystem;
